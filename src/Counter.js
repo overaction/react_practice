465751036e32b485b3f8fe59f0889a2a1d51b8c0
overaction <kmc04@naver.com>
2020-02-13 14:06:34 +0000

    Hooks #3 useReducer-1

--- src/Counter.js
+++ src/Counter.js
@@ -1,30 +1,31 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment, useReducer} from 'react';
 
-class Counter extends Component {
-    state = {
-        number: 0,
-        fixednumber : 0
-    };
-    render() {
-        const {number, fixednumber} = this.state;
-        return (
-            <Fragment>
-                <h1>{number}</h1>
-                <h2>{fixednumber}</h2>
-                <button onClick = { () => {
-                    this.setState(prev => {
-                        return {
-                            number: prev.number + 1
-                        }
-                    })
-                    this.setState(prev => ({
-                        number: prev.number + 1
-                    }))
-                }}> +1
-                </button>
-            </Fragment>
-        );
+function reducer(state, action) {
+    switch(action.type) {
+        case 'INCREMENT' :
+            return {val: state.val + 1};
+        case 'DECREMENT' :
+            return {val: state.val - 1};
+        default :
+            return state;
     }
+}
+
+const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, {val:0});
+    //useReducer 첫번째 파라미터 : 리듀서 함수
+    //useReducer 두번째 파라미터 : 리듀서의 기본 값
+    //state : 현재 상태, dispatch : 액션을 발생시키는 함수
+
+    return (
+        <Fragment>
+            <p>
+                현재 값 : <b>{state.val}</b>
+            </p>
+            <button onClick={() => dispatch({type: 'INCREMENT'})}>+1</button>
+            <button onClick={() => dispatch({type: 'DECREMENT'})}>-1</button>
+        </Fragment>
+    )
 }
 
 export default Counter;
