6a8d89588d184a218e1df72392a79709838f5be1
overaction <kmc04@naver.com>
2020-02-23 11:31:09 +0000

    Hooks #3 useMemo

--- src/Info.js
+++ src/Info.js
@@ -1,30 +1,29 @@
-import React, { useReducer, Fragment } from 'react';
-
-function reducer(state,action) {
-    return {
-        ...state,
-        [action.name]: action.value
-    }
-}
+import React, {useState, useEffect, Fragment} from 'react';
 
 const Info = () => {
-    const[state, dispatch] = useReducer(reducer, {
-        name: '',
-        nickname: ''
-    });
-    const {name,nickname} = state;
-    const onChange = e => {
-        dispatch(e.target); //reducer 함수 호출
+    const [name, setName] = useState('');
+    const [nickname, setNick] = useState('');
+    useEffect(() => {
+        console.log("렌더링 완료");
+        console.log({
+            name,nickname
+        })
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        }
+    },[])
+    
+    const onChangeName = e => {
+        setName(e.target.value);
     }
 
     return (
         <Fragment>
-            <input name="name" value={name} onChange={onChange} />
-            <input name="nickname" value={nickname} onChange={onChange} />
-            <br/>
-            <b>이름 : </b> {name}> <br/>
-            <b>닉네임 : </b> {nickname}> 
-
+            <input value={name} onChange={onChangeName} />
+            <input value={nickname} onChange={(e) => {setNick(e.target.value)}} /> <br />
+            <b>이름 : </b> {name} <br />
+            <b>닉네임: </b> {nickname} 
         </Fragment>
     )
 }
