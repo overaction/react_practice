ebaeef7688968e99b088dd94f4ef3125cc1b4b78
overaction <kmc04@naver.com>
2020-02-24 13:52:18 +0000

    Hooks #5 useRef

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment, useMemo, useCallback} from 'react';
+import React, {useState, Fragment, useMemo, useCallback, useRef} from 'react';
 
 const getAverage = (numbers) => {
     console.log('평균값 계산');
@@ -10,7 +10,7 @@ const getAverage = (numbers) => {
 const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
-
+    const inputEl = useRef(null);
     const onChange = useCallback(e => {
         setNumber(e.target.value);
     },[]) //컴포넌트가 처음 렌더링 될 때만 함수 생성
@@ -19,13 +19,14 @@ const Average = () => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
+        inputEl.current.focus();
     },[list,number]) //number 혹은 list가 변경되었을때만 함수 생성
 
     const avg = useMemo(() => getAverage(list),[list]);
 
     return (
     <Fragment>
-        <input value={number} onChange={onChange} />
+        <input value={number} onChange={onChange} ref={inputEl} />
         <button onClick={onInsert}>등록</button>
         <ul>
             {list.map((value, index) => (
