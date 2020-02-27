b5517f2a02f9eee573b451330596786e4ffde06a
overaction <kmc04@naver.com>
2020-02-27 14:30:17 +0000

    Todo_app #2 component styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoTemplate from './Todo_app/components/TodoTemplate';
 import TodoInsert from './Todo_app/components/TodoInsert';
+import TodoList from './Todo_app/components/TodoList';
 
 const App = () => {
     return (
         <TodoTemplate>
             <TodoInsert />
+            <TodoList />
         </TodoTemplate>
     )
 }
